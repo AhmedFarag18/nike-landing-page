89a77e2ea8cbd83cca53b1ef3d75381fee4f9148
AhmedFarag18 <ahmedfarag11_sd@fcis.bsu.edu.eg>
2025-01-14 20:25:00 +0200

    Upload main files

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Nav from './Nav';
+import Button from './Button';
+import PopularProductsCard from './PopularProductsCard';
+import ReviewCard from './ReviewCard';
+import ServiceCard from './ServiceCard';
+import ShoeCard from './ShoeCard';
+
+export { Nav, Button, PopularProductsCard, ReviewCard, ServiceCard, ShoeCard }
---
+++ src/sections/index.js
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+
+export {
+    Hero, CustomerReviews,
+    Services, SpecialOffers,
+    SuperQuality, Subscribe,
+    Footer, PopularProducts,
+}
